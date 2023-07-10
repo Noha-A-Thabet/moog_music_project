1409a4c749ee0ec4acc24b9d04c58a26b43341da
Noha-A-Thabet <NohaAshraf25@outlook.com>
2023-07-10 13:01:02 +0300

    "next/previous song , raise / mute volume"

--- js/Music.js
+++ js/Music.js
@@ -3,10 +3,37 @@ let controlIcon = document.getElementById("controlIcon");
 let pause = "fa-solid fa-pause";
 let play = "fa-solid fa-play";
 let controlDiv = document.getElementById("controlDiv");
+let nextBtn = document.getElementById("nextSong");
 let myAudio = document.getElementById("myAudio");
+let audioSource = myAudio.querySelector("source");
+let audioSrc = audioSource.getAttribute('src');
+let prevSong = document.getElementById("prevSong")
+let volume = document.getElementById("volume");
+let volumeRange = document.getElementById("volumeRange");
+let songImg = document.getElementById("songImg");
+let songName = document.getElementById("songName");
+let singer = document.getElementById("singer");
+let favourite = document.getElementById("favourite");
+
+// change volume range
+volumeRange.addEventListener("change", function volumeRange(e) {
+     let audioRange = parseFloat(e.target.value)
+     myAudio.volume = audioRange / 100;
+     if (myAudio.volume == 0) {
+          volume.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`
+     }
+
+})
+
+// mute  volume 
+volume.addEventListener("click", function muteSound() {
+     myAudio.muted = true;
+     volume.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`;
+     volumeRange.value = '0'
+})
+console.log(typeof +volumeRange.value)
 
 // play and pause song 
-
 function playPause() {
      if (controlIcon.classList.contains("fa-pause")) {
           myAudio.pause();
@@ -27,7 +54,8 @@ function playPause() {
 myAudio.onloadedmetadata = function () {
      progress.max = myAudio.duration;
      progress.value = myAudio.currentTime;
-}
+};
+
 if (myAudio.play()) {
      setInterval(() => {
           progress.value = myAudio.currentTime;
@@ -40,78 +68,122 @@ progress.onchange = function () {
 }
 
 
-let songSource = document.getElementById('songSource');
 let showSong = document.getElementById("showSong");
 
+async function getData() {
+     const url = 'https://spotify23.p.rapidapi.com/tracks/?ids=514rhnksEwHUh6LxXsQ4Y9%2C76OGwb5RA9h4FxQPT33ekc%2C6hgoYQDUcPyCz7LcTUHKxa%2C6zvHwijlnwqjS6d46yAffi%2C6z5Yh7kOKeLjqIsNdokIpU%2C';
+     const options = {
+          method: 'GET',
+          headers: {
+               'X-RapidAPI-Key': 'e42f066744msh00a021b82a2027ep1ecb87jsn20389b74f394',
+               'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+          }
+     };
+     try {
+
+          const response = await fetch(url, options);
+          const result = await response.json();
 
 
+          let index = 0;
+          if (result.tracks && result.tracks.length > 0) {
+               let song = result.tracks[index].preview_url;
+               let audioName = result.tracks[index].name;
+               let singerName = result.tracks[index].album.artists[0].name
+               let cover = result.tracks[index].album.images[0].url;
 
-showSong.addEventListener("click", function () {
-     console.log('next');
-     let nextAudio = document.createElement("audio");
-     let sourceElement = document.createElement("source");
-     sourceElement.setAttribute("src", "Paro Nej 128 Kbps.mp3");
-     showSong.appendChild(nextAudio)
-     nextAudio.appendChild(sourceElement);
-     nextAudio.play()
+               if (song) {
+                    audioSource.setAttribute("src", song);
+                    songImg.src = cover
+                    songName.textContent = audioName;
+                    singer.textContent = singerName;
+                    myAudio.load();
+                    myAudio.play();
 
-})
+                    // function addToFavourite 
+                    favourite.addEventListener("click", function addToFavourite(name, singer, img) {
+                         console.log(audioName, song, cover)
+                    })
 
-
-let songsList = [
-     {
-          id: 1,
-          song: './images/media/Calm-Down-Calm-Down(PaglaSongs).mp3',
-          name: "calm-Down",
-          singer: " Rema - Selena Gomez"
-     },
-     // {
-     //      id: 2,
-     //      song: '../media/Levitating Mp3 Song(NewDjSongRemix).mp3',
-     //      name: 'Levitating',
-     //      singer: "Dua Lipa"
-     // },
-     // {
-     //      id: 3,
-     //      song: '../media/Paro Nej 128 Kbps.mp3',
-     //      Name: 'Paro',
-     //      singer: "Nej"
-     // },
-     // {
-     //      id: 4,
-     //      song: '../media/Rosa_Linn_-_Snap_CeeNaija.com_.mp3',
-     //      name: 'SNAP',
-     //      singer: "Rosa Linn"
-     // },
-     // {
-     //      id: 5,
-     //      song: '../media/Christina_Perri_-_A_Thousand_Years_CeeNaija.com_.mp3',
-     //      name: 'A Thousand Years',
-     //      singer: 'Christina Perri',
-     // }
-];
-
-// for (let x = 0; x < songsList.length; x++) {
-
-//      songSource.src = songsList[x].song;
+                    // previous button
+                    prevSong.addEventListener("click", function () {
+                         index -= 1;
+                         if (index >= 0 && result.tracks[index]) {
+                              let prevSong = result.tracks[index].preview_url;
+                              audioSource.setAttribute("src", prevSong);
+                              let audioName = result.tracks[index].name;
+                              let singerName = result.tracks[index].album.artists[0].name
+                              let cover = result.tracks[index].album.images[0].url;
+                              songImg.src = cover
+                              songName.textContent = audioName;
+                              singer.textContent = singerName;
+                              myAudio.load();
+                              myAudio.play();
+                         } else {
+                              index = 0;
+                              let song = result.tracks[index].preview_url;
+                              if (song) {
+                                   audioSource.setAttribute("src", song);
+                                   myAudio.load();
+                                   myAudio.play();
+                              } else {
+                                   console.log("No songs available.");
+                              }
+                         }
+                    });
 
 
+                    // next song
+                    nextBtn.addEventListener("click", function () {
+                         index += 1;
+                         if (result.tracks[index]) {
+                              let nxtSong = result.tracks[index].preview_url;
+                              audioSource.setAttribute("src", nxtSong);
+                              let audioName = result.tracks[index].name;
+                              let singerName = result.tracks[index].album.artists[0].name
+                              let cover = result.tracks[index].album.images[0].url;
+                              songImg.src = cover
+                              songName.textContent = audioName;
+                              singer.textContent = singerName;
+                              myAudio.load();
+                              myAudio.play();
+                         } else {
+                              index = 0;
+                              song = result.tracks[index].preview_url;
+                              audioSource.setAttribute("src", song);
+                              myAudio.load();
+                              myAudio.play();
+                         }
+                    });
+               } else {
+                    console.log("Preview URL is missing or null.");
+               }
+          } else {
+               console.log("No tracks found in the response.");
+          }
 
-// }
-// console.log(audioSrc)
 
+          for (let x = 0; x < result.tracks.length; x++) {
 
+               // console.log(result.tracks[x])
+               // Album Name
+               let albumName = result.tracks[x].name;
+               // console.log("Album Name : " + albumName)
+               // artist name
+               let artistName = result.tracks[x].artists[0].name
+               // console.log("artist name : " + artistName)
+               // artist Image
+               let artistImage = result.tracks[x].album.images[0].url
+               // console.log(artistImage)
+               // song
 
-// function playPause() {
-//      myAudio.play();      // if (song.paused) {
-//      //      controlDiv.innerHTML = `<i class="fa-solid fa-pause"></i>`
-//      // } else {
-//      //      song.pause();
-//      //      controlDiv.innerHTML = `<i class="fa-solid fa-play"></i>`
-//      // }
-// }
+               // let song = result.tracks[x].preview_url;
+          }
 
-
+     } catch (error) {
+          console.log(error);
+     }
+} getData()
 
 
 
